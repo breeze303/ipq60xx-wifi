81a57d8cbea9390d820cda5512f562ece1b4b987
breeze303 <grapenskrskr@gmail.com>
2024-01-16 17:37:20 +0800

    git new

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -17,19 +17,6 @@ define Device/EmmcImage
 	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
 endef
 
-define Device/cmiot_ax18
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := CMIOT
-	DEVICE_MODEL := AX18
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@cp03-c1
-	SOC := ipq6018
-	DEVICE_PACKAGES := ipq-wifi-cmiot_ax18 kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
-endef
-TARGET_DEVICES += cmiot_ax18
-
 define Device/glinet_gl-ax1800
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
@@ -58,10 +45,8 @@ define Device/glinet_gl-axt1800
 endef
 TARGET_DEVICES += glinet_gl-axt1800
 
-
 define Device/jdc_ax1800-pro
 	$(call Device/FitImage)
-	$(call Device/EmmcImage)
 	DEVICE_VENDOR := JD Cloud
 	DEVICE_MODEL := JDC AX1800 Pro
 	DEVICE_DTS_CONFIG := config@cp03-c2
@@ -69,9 +54,9 @@ define Device/jdc_ax1800-pro
 	SOC := ipq6018
 	DEVICE_PACKAGES := ipq-wifi-jdc_ax1800-pro kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
 	BLOCKSIZE := 64k
-	IMAGES += kernel.bin rootfs.bin
-	IMAGE/kernel.bin := append-kernel
-	IMAGE/rootfs.bin := append-rootfs | pad-rootfs | pad-to $$(BLOCKSIZE)
+	KERNEL_SIZE := 6144k
+	IMAGES += factory.bin
+	IMAGE/factory.bin := append-kernel | pad-to 6144k |  append-rootfs | append-metadata
 endef
 TARGET_DEVICES += jdc_ax1800-pro
 
@@ -83,6 +68,9 @@ define Device/redmi_ax5-jdcloud
 	DEVICE_DTS_CONFIG := config@cp03-c1
 	SOC := ipq6018
 	DEVICE_PACKAGES := ipq-wifi-redmi_ax5-jdcloud
+	#BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-rootfs | append-metadata
 endef
 TARGET_DEVICES += redmi_ax5-jdcloud
 
@@ -111,29 +99,3 @@ define Device/xiaomi_ax1800
 	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax1800
 endef
 TARGET_DEVICES += xiaomi_ax1800
-
-define Device/zn_m2
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := ZN
-	DEVICE_MODEL := M2
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@cp03-c1
-	SOC := ipq6018
-	DEVICE_PACKAGES := ipq-wifi-zn_m2
-endef
-TARGET_DEVICES += zn_m2
-
-define Device/qihoo_v6
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := Qihoo
-	DEVICE_MODEL := V6
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@cp03-c1
-	SOC := ipq6018
-	DEVICE_PACKAGES := ipq-wifi-qihoo_v6
-endef
-TARGET_DEVICES += qihoo_v6
